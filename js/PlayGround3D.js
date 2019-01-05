3225f6412e648defcb89e88fee7c49052a93be65
worldmaker <worldmaker18349276@gmail.com>
2019-01-06 02:59:12 +0800

    add playable class

--- js/PlayGround3D.js
+++ js/PlayGround3D.js
@@ -1,188 +1,189 @@
 "use strict";
 
-function Display(id, width, height) {
-  var parent = document.getElementById(id);
-  this.width = width;
-  this.height = height;
+class Display
+{
+  constructor(id, width, height) {
+    var parent = document.getElementById(id);
+    this.width = width;
+    this.height = height;
 
-  // renderer
-  this.renderer = new THREE.WebGLRenderer({antialias:true});
-  this.renderer.setSize(width, height);
-  this.dom = this.renderer.domElement;
-  parent.appendChild(this.dom);
+    // renderer
+    this.renderer = new THREE.WebGLRenderer({antialias:true});
+    this.renderer.setSize(width, height);
+    this.dom = this.renderer.domElement;
+    parent.appendChild(this.dom);
 
-  this.scene = new THREE.Scene();
-  this.scene.add(new THREE.AmbientLight(0xffffff, 0.3));
-  this.scene.add(new THREE.AxesHelper(20));
-  this.scene.add(this.buildBackground());
+    this.scene = new THREE.Scene();
+    this.scene.add(new THREE.AmbientLight(0xffffff, 0.3));
+    this.scene.add(new THREE.AxesHelper(20));
+    this.scene.add(this.buildBackground());
 
-  // navigation control
-  this.rotateSpeed = Math.PI/500;
-  this.zoomSpeed = 1/100;
-  this.distanceRange = [2, 8];
-  this.trackball_lock = new Set();
+    // navigation control
+    this.rotateSpeed = Math.PI/500;
+    this.zoomSpeed = 1/100;
+    this.distanceRange = [2, 8];
+    this.trackball_lock = new Set();
 
-  this.trackball = new THREE.Group();
-  this.camera = new THREE.PerspectiveCamera(40, width/height, 1, 1000);
-  this.camera.add(new THREE.PointLight(0xffffff, 0.7));
-  this.trackball.add(this.camera);
-  this.scene.add(this.trackball);
-  this.setCamera(1,2,3);
+    this.trackball = new THREE.Group();
+    this.camera = new THREE.PerspectiveCamera(40, width/height, 1, 1000);
+    this.camera.add(new THREE.PointLight(0xffffff, 0.7));
+    this.trackball.add(this.camera);
+    this.scene.add(this.trackball);
+    this.setCamera(1,2,3);
 
-  document.addEventListener("contextmenu", event => {
-    if ( event.target === this.dom )
-      event.preventDefault();
-  });
-  this.dom.addEventListener("mousemove", event => {
-    if ( this.trackball_lock.size ) return;
-
-    var x = event.movementX, y = -event.movementY;
-
-    switch ( event.buttons ) {
-      case 1: // rotate
-        let axis = new THREE.Vector3(-y,x,0).normalize();
-        let angle = Math.sqrt(x*x+y*y)*this.rotateSpeed;
-        this.trackball.rotateOnAxis(axis, -angle);
+    document.addEventListener("contextmenu", event => {
+      if ( event.target === this.dom )
         event.preventDefault();
-        break;
+    });
+    this.dom.addEventListener("mousemove", event => {
+      if ( this.trackball_lock.size ) return;
 
-      case 2: // spin
-        let theta = x*this.rotateSpeed;
-        this.trackball.rotateZ(theta);
-        event.preventDefault();
-        break;
+      var x = event.movementX, y = -event.movementY;
 
-      case 4: // zoom
-        let z = this.camera.position.z - y*this.zoomSpeed;
-        if ( this.distanceRange[0] < z && z < this.distanceRange[1] )
-          this.camera.position.z = z;
-        event.preventDefault();
-        break;
-    }
-  }, false);
+      switch ( event.buttons ) {
+        case 1: // rotate
+          let axis = new THREE.Vector3(-y,x,0).normalize();
+          let angle = Math.sqrt(x*x+y*y)*this.rotateSpeed;
+          this.trackball.rotateOnAxis(axis, -angle);
+          event.preventDefault();
+          break;
 
-  // animation
-  {
-    this.animations = [];
-    let animation_loop = (t) => {
-      requestAnimationFrame(animation_loop);
-      for ( let i=0; i<this.animations.length; i++ ) {
-        let [coroutine, resolve, reject] = this.animations[i];
-        try {
-          let result = coroutine.next(t);
-          if ( result.done ) {
-            resolve();
-            this.animations.splice(i, 1);
-            i--;
+        case 2: // spin
+          let theta = x*this.rotateSpeed;
+          this.trackball.rotateZ(theta);
+          event.preventDefault();
+          break;
+
+        case 4: // zoom
+          let z = this.camera.position.z - y*this.zoomSpeed;
+          if ( this.distanceRange[0] < z && z < this.distanceRange[1] )
+            this.camera.position.z = z;
+          event.preventDefault();
+          break;
+      }
+    }, false);
+
+    // animation
+    {
+      this.animations = [];
+      let animation_loop = (t) => {
+        requestAnimationFrame(animation_loop);
+        for ( let i=0; i<this.animations.length; i++ ) {
+          let [coroutine, resolve, reject] = this.animations[i];
+          try {
+            let result = coroutine.next(t);
+            if ( result.done ) {
+              resolve();
+              this.animations.splice(i, 1);
+              i--;
+            }
+          } catch ( e ) {
+            reject(e);
           }
-        } catch ( e ) {
-          reject(e);
         }
-      }
-      this.renderer.render(this.scene, this.camera);
-    };
-    requestAnimationFrame(animation_loop);
-  }
+        this.renderer.render(this.scene, this.camera);
+      };
+      requestAnimationFrame(animation_loop);
+    }
 
 
-  // event system on Object3D
-  this.raycaster = new THREE.Raycaster();
-  this.raycaster.linePrecision = 0;
-  function event3D(name, event, point) {
-    return Object.assign({type:name, originalEvent:event}, point);
-  }
+    // event system on Object3D
+    this.raycaster = new THREE.Raycaster();
+    this.raycaster.linePrecision = 0;
+    function event3D(name, event, point) {
+      return Object.assign({type:name, originalEvent:event}, point);
+    }
 
-  // hover event
-  {
-    let hover_spot = {};
+    // hover event
+    {
+      let hover_spot = {};
 
-    this.dom.addEventListener("mousemove", event => {
-      var prev_target3D = hover_spot.object;
-      hover_spot = this.pointTo(event, e => e.userData.hoverable);
-      var target3D = hover_spot.object;
+      this.dom.addEventListener("mousemove", event => {
+        var prev_target3D = hover_spot.object;
+        hover_spot = this.pointTo(event, e => e.userData.hoverable);
+        var target3D = hover_spot.object;
 
-      if ( prev_target3D !== target3D ) {
-        if ( prev_target3D )
-          prev_target3D.dispatchEvent(event3D("mouseleave", event, hover_spot));
+        if ( prev_target3D !== target3D ) {
+          if ( prev_target3D )
+            prev_target3D.dispatchEvent(event3D("mouseleave", event, hover_spot));
+          if ( target3D )
+            target3D.dispatchEvent(event3D("mouseenter", event, hover_spot));
+        }
         if ( target3D )
-          target3D.dispatchEvent(event3D("mouseenter", event, hover_spot));
-      }
-      if ( target3D )
-        target3D.dispatchEvent(event3D("mouseover", event, hover_spot));
-    }, false);
-  }
+          target3D.dispatchEvent(event3D("mouseover", event, hover_spot));
+      }, false);
+    }
 
-  // click event
-  {
-    let click_spot = {};
+    // click event
+    {
+      let click_spot = {};
 
-    this.dom.addEventListener("mousedown", event => {
-      if ( !click_spot.object ) {
-        click_spot = this.pointTo(event, e => e.userData.hoverable);
-      }
-    }, false);
-    this.dom.addEventListener("mousemove", event => {
-      if ( click_spot.object ) {
-        if ( Math.abs(event.movementX) > 1 || Math.abs(event.movementY) > 1 )
-          click_spot = {};
-      }
-    }, false);
-    this.dom.addEventListener("mouseup", event => {
-      if ( click_spot.object ) {
-        click_spot.object.dispatchEvent(event3D("click", event, click_spot));
-        click_spot = {};
-      }
-    }, false);
-  }
-
-  // drag event
-  {
-    let dragging = false, drag_spot = {};
-    const DRAG_KEY = Symbol("drag");
-
-    this.dom.addEventListener("mousedown", event => {
-      if ( event.buttons === 1 && !dragging ) {
-        drag_spot = this.pointTo(event, e => e.userData.hoverable);
-        if ( drag_spot.object && !drag_spot.object.userData.draggable )
-          drag_spot = {};
-        if ( drag_spot.object )
-          this.lockTrackball(DRAG_KEY);
-      }
-    }, false);
-    this.dom.addEventListener("mousemove", event => {
-      if ( event.buttons === 1 ) {
-        var target3D = drag_spot.object;
-
-        if ( !dragging && target3D ) {
-          target3D.dispatchEvent(event3D("dragstart", event, drag_spot));
-          dragging = true;
+      this.dom.addEventListener("mousedown", event => {
+        if ( !click_spot.object ) {
+          click_spot = this.pointTo(event, e => e.userData.hoverable);
         }
+      }, false);
+      this.dom.addEventListener("mousemove", event => {
+        if ( click_spot.object ) {
+          if ( Math.abs(event.movementX) > 1 || Math.abs(event.movementY) > 1 )
+            click_spot = {};
+        }
+      }, false);
+      this.dom.addEventListener("mouseup", event => {
+        if ( click_spot.object ) {
+          click_spot.object.dispatchEvent(event3D("click", event, click_spot));
+          click_spot = {};
+        }
+      }, false);
+    }
 
-        if ( dragging )
-          target3D.dispatchEvent(event3D("drag", event, {}));
-      }
-    }, false);
-    this.dom.addEventListener("mouseup", event => {
-      if ( dragging ) {
-        drag_spot.object.dispatchEvent(event3D("dragend", event, {}));
-        this.unlockTrackball(DRAG_KEY);
-        drag_spot = {};
-        dragging = false;
-      }
-    }, false);
+    // drag event
+    {
+      let dragging = false, drag_spot = {};
+      const DRAG_KEY = Symbol("drag");
+
+      this.dom.addEventListener("mousedown", event => {
+        if ( event.buttons === 1 && !dragging ) {
+          drag_spot = this.pointTo(event, e => e.userData.hoverable);
+          if ( drag_spot.object && !drag_spot.object.userData.draggable )
+            drag_spot = {};
+          if ( drag_spot.object )
+            this.lockTrackball(DRAG_KEY);
+        }
+      }, false);
+      this.dom.addEventListener("mousemove", event => {
+        if ( event.buttons === 1 ) {
+          var target3D = drag_spot.object;
+
+          if ( !dragging && target3D ) {
+            target3D.dispatchEvent(event3D("dragstart", event, drag_spot));
+            dragging = true;
+          }
+
+          if ( dragging )
+            target3D.dispatchEvent(event3D("drag", event, {}));
+        }
+      }, false);
+      this.dom.addEventListener("mouseup", event => {
+        if ( dragging ) {
+          drag_spot.object.dispatchEvent(event3D("dragend", event, {}));
+          this.unlockTrackball(DRAG_KEY);
+          drag_spot = {};
+          dragging = false;
+        }
+      }, false);
+    }
   }
-}
-Object.assign(Display.prototype, {
-  add: function(target, parent=this.scene) {
+  add(target, parent=this.scene) {
     parent.add(target);
     target.dispatchEvent({name:"objectadded", target, parent});
-  },
-  remove: function(target, parent=this.scene) {
+  }
+  remove(target, parent=this.scene) {
     target.dispatchEvent({name:"objectremoved", target, parent});
     parent.remove(target);
-  },
+  }
 
-  buildBackground: function() {
+  buildBackground() {
     var background_geometry = new THREE.IcosahedronGeometry(10, 0);
     var background_uv = [new THREE.Vector2(0,0), new THREE.Vector2(0,1), new THREE.Vector2(Math.sqrt(3)/2,0)];
     for ( let i in background_geometry.faceVertexUvs[0] )
@@ -196,9 +197,9 @@ Object.assign(Display.prototype, {
     background_texture.repeat = new THREE.Vector2(2.5, 2.5);
     background_material.map = background_texture;
     return new THREE.Mesh(background_geometry, background_material);
-  },
+  }
 
-  pointTo: function(event, objs) {
+  pointTo(event, objs) {
     if ( typeof objs == "function" ) {
       let filter = objs;
       objs = [];
@@ -206,8 +207,8 @@ Object.assign(Display.prototype, {
     }
 
     var mouse = new THREE.Vector2(
-      event.clientX/this.width*2 - 1,
-      - event.clientY/this.height*2 + 1);
+      event.offsetX/this.width*2 - 1,
+      - event.offsetY/this.height*2 + 1);
     this.raycaster.setFromCamera(mouse, this.camera);
 
     if ( Array.isArray(objs) ) {
@@ -224,9 +225,9 @@ Object.assign(Display.prototype, {
     } else {
       throw "??";
     }
-  },
+  }
 
-  setCamera: function(x, y, z, dis) {
+  setCamera(x, y, z, dis) {
   	var vec = new THREE.Vector3(x, y, z);
     if ( !dis )
       dis = vec.length();
@@ -235,20 +236,20 @@ Object.assign(Display.prototype, {
 
     this.camera.position.z = dis;
     this.trackball.lookAt(vec.normalize());
-  },
-  lockTrackball: function(key) {
+  }
+  lockTrackball(key) {
     this.trackball_lock.add(key);
-  },
-  unlockTrackball: function(key) {
+  }
+  unlockTrackball(key) {
     this.trackball_lock.delete(key);
-  },
+  }
 
-  animate: function(coroutine) {
+  animate(coroutine) {
     return new Promise((resolve, reject) => {
       this.animations.push([coroutine, resolve, reject]);
     });
-  },
-  animatedRotateRoutine: function*(targets, axis, angle, speed) {
+  }
+  *animatedRotateRoutine(targets, axis, angle, speed) {
     var start_quaternions = targets.map(target => target.quaternion.clone());
     var curr_angle = 0;
     var t0, t;
@@ -263,6 +264,6 @@ Object.assign(Display.prototype, {
       for ( let i in targets )
         targets[i].quaternion.multiplyQuaternions(rot, start_quaternions[i]);
     }
-  },
-});
+  }
+}
 
---
+++ js/SlidingSpherePuzzle.js
@@ -0,0 +1,237 @@
+function *colors() {
+  while ( true ) {
+    // ref: https://sashat.me/2017/01/11/list-of-20-simple-distinct-colors/
+    yield *["#e6194B", "#3cb44b",
+            "#ffe119", "#4363d8",
+            "#f58231", "#911eb4",
+            "#42d4f4", "#f032e6",
+            "#bfef45", "#fabebe",
+            "#469990", "#e6beff",
+            "#9A6324", "#fffac8",
+            "#800000", "#aaffc3",
+            "#808000", "#ffd8b1",
+            "#000075", "#a9a9a9",
+            "#ffffff", "#000000"];
+  }
+}
+var cloop = colors();
+
+class SSphPuzzle
+{
+  constructor(id, width, height) {
+    this.display = new Display(id, width, height);
+    this.elements = [new SphElem()];
+    this.gui = new dat.GUI();
+
+    this.ball = this.buildBallView(0.999);
+    this.display.add(this.ball);
+
+    // build selection system
+    var sel_gui = this.sel_gui = this.gui.addFolder("selected segment");
+    sel_gui.open();
+
+    this.selection = {length:0, radius:0, angle:0, host:undefined};
+    this.sel_ctrl = {};
+
+    this.selection.object = () => console.log(this.selection.host);
+    this.sel_ctrl.object = sel_gui.add(this.selection, "object");
+
+    this.sel_ctrl.length = sel_gui.add(this.selection, "length", 0, 4, 0.01);
+    this.sel_ctrl.radius = sel_gui.add(this.selection, "radius", 0, 2, 0.01);
+    this.sel_ctrl.angle = sel_gui.add(this.selection, "angle", 0, 4, 0.01);
+    this.sel_ctrl.length.domElement.style.pointerEvents = "none";
+    this.sel_ctrl.radius.domElement.style.pointerEvents = "none";
+    this.sel_ctrl.angle.domElement.style.pointerEvents = "none";
+
+    this.selection.links = {};
+    this.sel_ctrl.links = {};
+
+    this.select();
+  }
+
+  emphasize(seg) {
+    seg.view[0].material.linewidth = 2;
+    seg.view[1].material.linewidth = 2;
+  }
+  unemphasize(seg) {
+    seg.view[0].material.linewidth = 1;
+    seg.view[1].material.linewidth = 1;
+  }
+  highlight(seg) {
+    seg.view[3].material.opacity = 0.3;
+  }
+  unhighlight(seg) {
+    seg.view[3].material.opacity = 0;
+  }
+  select(seg) {
+    // remove previous selection
+    var prev = this.selection.host;
+    this.selection.host = undefined;
+
+    if ( prev ) {
+      this.unemphasize(prev);
+      this.unhighlight(prev);
+    }
+
+    this.selection.length = NaN;
+    this.selection.radius = NaN;
+    this.selection.angle = NaN;
+
+    for ( let name in this.sel_ctrl.links )
+      this.sel_ctrl.links[name].remove();
+    this.selection.links = {};
+    this.sel_ctrl.links = {};
+
+    // set present selection
+    if ( seg ) {
+      this.selection.host = seg;
+
+      this.highlight(seg);
+
+      this.selection.length = seg.length;
+      this.selection.radius = seg.radius;
+      this.selection.angle = seg.angle;
+
+      for ( let name of ["prev", "next"] )
+        this.makeLink(name, seg[name]);
+      for ( let [adj_seg, offset] of seg.adj )
+        this.makeLink(`adj(${offset})`, adj_seg);
+    }
+
+    this.sel_ctrl.length.updateDisplay();
+    this.sel_ctrl.radius.updateDisplay();
+    this.sel_ctrl.angle.updateDisplay();
+  }
+  makeLink(name, seg) {
+    this.selection.links[name] = () => this.select(seg);
+    this.sel_ctrl.links[name] = this.sel_gui.add(this.selection.links, name);
+    var dom = this.sel_ctrl.links[name].domElement.parentNode.parentNode;
+    dom.addEventListener("mouseenter", () => this.emphasize(seg));
+    dom.addEventListener("mouseleave", () => this.unemphasize(seg));
+  }
+
+  show() {
+    this.view = [];
+    for ( let elem of this.elements )
+      this.view.push(this.buildElemView(elem, cloop.next().value));
+
+    for ( let elem of this.view )
+      this.display.add(elem);
+  }
+
+  buildBallView(R=1, N=5) {
+    var geo = new THREE.IcosahedronGeometry(R, N);
+    var mat = new THREE.MeshLambertMaterial({color:0xffffff});
+
+    var ball = new THREE.Mesh(geo, mat);
+
+    ball.userData.hoverable = true;
+    ball.addEventListener("click", () => this.select());
+
+    return ball;
+  }
+  buildElemView(elem, color, dq=0.01) {
+    var obj = new THREE.Object3D();
+    obj.userData.data = elem;
+    elem.view = obj;
+
+    for ( let seg of elem.children )
+      obj.add(...this.buildSegView(seg, dq, color));
+
+    return obj;
+  }
+  buildSegView(seg, dq, color) {
+    // make arc
+    {
+      let geo = new THREE.Geometry();
+      let circle = seg.circle;
+      let center = new THREE.Vector3(...circle.center);
+      let v0 = new THREE.Vector3(...circle.vectorAt(0));
+      let da = dq*Math.sin(circle.radius*Q);
+      geo.vertices = Array.from({length:Math.floor(seg.length/da)+1},
+                                (_, i) => v0.clone().applyAxisAngle(center, i*da*Q));
+      geo.vertices.push(new THREE.Vector3(...circle.vectorAt(seg.length)));
+
+      let mat = new THREE.LineBasicMaterial({color});
+      var arc = new THREE.Line(geo, mat);
+      arc.name = "arc";
+    }
+
+    // make dash
+    {
+      let geo = new THREE.Geometry();
+      let circle = new SphCircle({radius:seg.radius-dq/2, orientation:seg.orientation});
+      let center = new THREE.Vector3(...circle.center);
+      let v0 = new THREE.Vector3(...circle.vectorAt(0));
+      let da = dq*Math.sin(circle.radius*Q);
+      geo.vertices = Array.from({length:Math.floor(seg.length/da)-1},
+                                (_, i) => v0.clone().applyAxisAngle(center, (i+1)*da*Q));
+
+      let mat = new THREE.LineDashedMaterial({color, dashSize: dq*Q/2, gapSize: dq*Q/2});
+      var dash = new THREE.Line(geo, mat);
+      dash.computeLineDistances();
+      dash.name = "dash";
+    }
+
+    // make angle
+    {
+      let geo;
+      if ( fzy_cmp(seg.angle, 0) != 0 )
+        geo = new THREE.CircleGeometry(dq*Q, 10, Q, seg.angle*Q);
+      else
+        geo = new THREE.CircleGeometry(3*dq*Q, 3, (1-10*dq)*Q, 2*10*dq*Q);
+      geo.translate(0, 0, 1);
+      geo.rotateY(seg.radius*Q);
+      let q = new THREE.Quaternion(...seg.orientation);
+      geo.applyMatrix(new THREE.Matrix4().makeRotationFromQuaternion(q));
+
+      let mat = new THREE.MeshBasicMaterial({color});
+      var ang = new THREE.Mesh(geo, mat);
+      ang.name = "angle";
+    }
+
+    // make holder and register event listeners
+    {
+      let N = Math.floor(seg.length/(dq*Math.sin(seg.radius*Q)))+1;
+      let geo = new THREE.CylinderGeometry(
+        Math.sin((seg.radius-dq)*Q), Math.sin(seg.radius*Q), Math.sin(seg.radius*Q)*dq,
+        N, 1, true, Q, seg.length*Q);
+      geo.rotateX(Q);
+      geo.translate(0, 0, Math.cos((seg.radius-dq/2)*Q));
+      let q = new THREE.Quaternion(...seg.orientation);
+      geo.applyMatrix(new THREE.Matrix4().makeRotationFromQuaternion(q));
+
+      let mat = new THREE.MeshBasicMaterial({ color, transparent: true, opacity: 0 });
+      var holder = new THREE.Mesh(geo, mat);
+      holder.name = "holder";
+
+      holder.userData.hoverable = true;
+      holder.addEventListener("mouseenter", () => this.emphasize(seg));
+      holder.addEventListener("mouseleave", () => this.unemphasize(seg));
+      holder.addEventListener("click", () => this.select(seg));
+    }
+
+    seg.view = [arc, dash, ang, holder];
+    return [arc, dash, ang, holder];
+  }
+
+  slice(center, radius) {
+    var circle = new SphCircle({center, radius});
+    var new_elems = [];
+    for ( let elem of this.elements ) {
+      let [in_segs, out_segs] = elem.slice(circle);
+      new_elems.push(...elem.split(in_segs, out_segs));
+    }
+    this.elements = new_elems;
+  }
+  mergeTrivialVertices() {
+    for ( let elem of this.elements ) {
+      elem.mergeTrivialVertices();
+    }
+  }
+  build_twist() {
+    for ( let elem of this.elements )
+      for ( let seg of elem.children )
+        SphLock.build(seg);
+  }
+}
